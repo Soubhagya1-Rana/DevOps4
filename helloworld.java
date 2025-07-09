cb6716c93ae97740bc6cbafde45f0665afc28b48
Soubhagya1-Rana <67109059+Soubhagya1-Rana@users.noreply.github.com>
2025-07-09 17:06:16 +0530

    helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,24 @@
+class Animal {
+    public void speak() {
+        System.out.println("Animal speaks");
+    }
+}
+
+class Dog extends Animal {
+    public void speak() {
+        System.out.println("Dog barks");
+    }
+
+    public void fetch() {
+        System.out.println("Dog fetches");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.speak();
+        // a.fetch(); // What happens if this line is uncommented?
+    }
+}
 
